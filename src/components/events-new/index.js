d75c5d6938e267bbdf41e4755bafb8bae7ef91b4
Hardik Narang <126074137+Hardik-2311@users.noreply.github.com>
2024-02-25 22:25:33 +0530

    Add new events component for home screen (#34)
    * new component for events and commented out facilities and authorities
    
    * made new eventCard
    
    * changed EventCard to class
    
    * added line b/w imports
    
    ---------
    
    Co-authored-by: Hardik <naranghardik1903@gmaik.com>

---
+++ src/components/events-new/index.js
@@ -0,0 +1,55 @@
+import React, { Component, lazy } from 'react';
+
+import { tailwindWrapper } from "../../../../../formula_one/src/utils/tailwindWrapper";
+
+const EventCard = lazy(() => import("../events-card-new/index"))
+
+class NewEvents extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      max_length: 3,
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ loading: false });
+    }, 2000);
+  }
+
+  handleRegister = (eventName) => {
+    alert(`You have registered for ${eventName}`);
+  }
+
+  render() {
+    const { loading } = this.state;
+    return (
+      <div className={tailwindWrapper("container mx-auto mb-4")}>
+        <h2 className={tailwindWrapper("text-2xl text-[#133BC5] font-bold mb-2")}>Events</h2>
+        <div className={tailwindWrapper("flex justify-between mb-2")}>
+          <p className={tailwindWrapper("text-lg font-bold")}>Upcoming Bhawan Events</p>
+          <p className={tailwindWrapper("font-bold text-[#133BC5]")}>View More</p>
+        </div>
+        {!loading ? (
+          <div className={tailwindWrapper("flex flex-col space-y-2")}>
+            {[...Array(this.state.max_length)].map((_, index) => (
+              <EventCard
+                key={index}
+                eventName={`Event ${index + 1}`}
+                eventLocation={`Location ${index + 1}`}
+                onRegister={() => this.handleRegister(`Event ${index + 1}`)}
+                index={index}
+              />
+            ))}
+          </div>
+        ) : (
+          <div>Loading...</div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default NewEvents;
